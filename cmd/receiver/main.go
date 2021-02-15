9f14a644e13170c7d13b8b91e2af85108066adb2
Kevin Wang <kevmo314@gmail.com>
2021-02-14 20:59:27 -0500

    changes

--- cmd/receiver/main.go
+++ cmd/receiver/main.go
@@ -24,7 +24,7 @@ func showHelp() {
 
 func parse() bool {
 	flag.StringVar(&input, "i", "127.0.0.1:1936", "address to listen on")
-	flag.StringVar(&output, "o", "127.0.0.1:1937", "address to write to")
+	flag.StringVar(&output, "o", "127.0.0.1:1935", "address to write to")
 	help := flag.Bool("h", false, "help info")
 	flag.Parse()
 
--- deployments/receiver.Dockerfile
+++ deployments/receiver.Dockerfile
@@ -3,7 +3,10 @@ FROM golang:1.15
 WORKDIR /go/src/app
 COPY . .
 
+ENV INPUT_ADDRESS ":1936"
+ENV OUTPUT_ADDRESS "rtmp:1935"
+
 RUN go mod download
 RUN go build -o ./receiver cmd/receiver/main.go
 
-CMD ["/go/src/app/receiver"]
+CMD ["/go/src/app/receiver", "-i", "${INPUT}", "-o", "${OUTPUT}"]
