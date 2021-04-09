d0e832081291782ff6bcc25c730cb2881e7981c7
Kevin Wang <kevmo314@gmail.com>
2021-04-09 23:20:30 +0000

    undo

--- pkg/muxer/muxer.go
+++ pkg/muxer/muxer.go
@@ -18,7 +18,7 @@ type Muxer struct {
 
 // NewMuxer creates a new uniplex listener muxer
 func NewMuxer(options ...func(*Muxer)) *Muxer {
-	buf := buffer.NewReceiverBuffer(1500 * time.Millisecond)
+	buf := buffer.NewReceiverBuffer(300 * time.Millisecond)
 	m := &Muxer{buf: buf, responseCh: make(chan []byte, 128)}
 
 	for _, option := range options {
@@ -50,21 +50,7 @@ func (m *Muxer) writeLoop(dial *net.UDPAddr) {
 				break
 			}
 
-			p, err := srt.Unmarshal(msg[:n])
-			if err != nil {
-				fmt.Printf("failed to unmarshal response %v\n", err)
-				continue
-			}
-
-			switch v := p.(type) {
-			case *srt.ControlPacket:
-				if v.ControlType() != srt.ControlTypeNak {
-					// only send non-nak's upstream.
-					m.responseCh <- p.Marshal()
-				}
-			case *srt.DataPacket:
-				m.responseCh <- p.Marshal()
-			}
+			m.responseCh <- msg[:n]
 		}
 	}()
 
