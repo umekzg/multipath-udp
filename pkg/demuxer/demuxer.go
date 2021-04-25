9b9eae44fc0af42d5cc3d84d38027b503190ced2
Kevin Wang <kevmo314@gmail.com>
2021-04-25 02:22:02 +0000

    wtf #1

--- pkg/demuxer/demuxer.go
+++ pkg/demuxer/demuxer.go
@@ -3,10 +3,8 @@ package demuxer
 import (
 	"fmt"
 	"net"
-	"sync"
 
 	"github.com/muxfd/multipath-udp/pkg/interfaces"
-	"github.com/muxfd/multipath-udp/pkg/srt"
 )
 
 // Demuxer represents a UDP stream demuxer that demuxes a source over multiple senders.
@@ -39,53 +37,13 @@ func (d *Demuxer) readLoop(listen, dial *net.UDPAddr) {
 	r.SetReadBuffer(1024)
 	r.SetWriteBuffer(1024)
 
-	var sourceLock sync.Mutex
-	sources := make(map[uint32]*net.UDPAddr)
 	interfaces := NewInterfaceSet(dial, d.responseCh)
 	if d.interfaceBinder != nil {
 		close := d.interfaceBinder.Bind(interfaces.Add, interfaces.Remove, dial)
 		defer close()
 	}
 
-	rates := make(map[string]uint32)
-	var rateLock sync.Mutex
-
-	go func() {
-		for {
-			msg, ok := <-d.responseCh
-			if !ok {
-				fmt.Printf("response channel closed\n")
-				break
-			}
-			p, err := srt.Unmarshal(msg.msg)
-			if err != nil {
-				fmt.Printf("error unmarshaling packet: %v\n", err)
-				continue
-			}
-			switch v := p.(type) {
-			case *srt.ControlPacket:
-				if v.ControlType() == srt.ControlTypeNak {
-					fmt.Printf("recv nak\n")
-				}
-				if v.ControlType() == srt.ControlTypeUserDefined && v.Subtype() == srt.SubtypeMultipathAck {
-					rateLock.Lock()
-					fmt.Printf("%v %v\n", msg.addr, v.TypeSpecificInformation())
-					rates[msg.addr] = v.TypeSpecificInformation()
-					rateLock.Unlock()
-				}
-			}
-			sourceLock.Lock()
-			// this is kind of weird.
-			d := make([]byte, len(msg.msg))
-			copy(d, msg.msg)
-			if source, ok := sources[p.DestinationSocketId()]; ok {
-				if _, err := r.WriteToUDP(d, source); err != nil {
-					fmt.Printf("error writing to udp: %v\n", err)
-				}
-			}
-			sourceLock.Unlock()
-		}
-	}()
+	sessions := make(map[string]chan []byte)
 
 	for {
 		msg := make([]byte, 1500)
@@ -96,47 +54,89 @@ func (d *Demuxer) readLoop(listen, dial *net.UDPAddr) {
 			break
 		}
 
-		p, err := srt.Unmarshal(msg[:n])
-		if err != nil {
-			fmt.Printf("error unmarshaling srt packet %v\n", err)
-			return
-		}
-		if p.DestinationSocketId() == 0 {
-			switch v := p.(type) {
-			case *srt.ControlPacket:
-				sourceLock.Lock()
-				sources[v.HandshakeSocketId()] = senderAddr
-				sourceLock.Unlock()
-			}
-		}
-		if err != nil {
-			fmt.Printf("error unmarshalling rtp packet %v\n", err)
-			return
-		}
+		if session, ok := sessions[senderAddr.String()]; ok {
+			session <- msg[:n]
+		} else {
+			reqCh := make(chan []byte, 128)
+			sessions[senderAddr.String()] = reqCh
+			session <- msg[:n]
 
-		conns := interfaces.Connections()
-		if len(conns) == 0 {
-			fmt.Printf("no connections available\n")
-			return
-		}
-
-		switch v := p.(type) {
-		case *srt.DataPacket:
-			fmt.Printf("sending pkt %d\n", v.SequenceNumber())
-			// write to all interfaces.
-			for _, conn := range conns {
-				if _, err := conn.Write(msg[:n]); err != nil {
-					fmt.Printf("error writing to socket %v: %v\n", conn, err)
+			go func(reqCh chan []byte, senderAddr *net.UDPAddr) {
+				conns := interfaces.Connections()
+				if len(conns) == 0 {
+					fmt.Printf("no connections available\n")
+					return
 				}
-			}
-		case *srt.ControlPacket:
-			// pick a random connection.
-			fmt.Printf("control pkt\n")
-			conn := conns[0]
-			if _, err := conn.Write(msg[:n]); err != nil {
-				fmt.Printf("error writing to socket %v: %v\n", conn, err)
-			}
+
+				go func() {
+					for {
+						msg, ok := <-reqCh
+						if !ok {
+							break
+						}
+						for _, conn := range conns {
+							if _, err := conn.Write(msg); err != nil {
+								fmt.Printf("error writing to socket %v: %v\n", conn, err)
+							}
+						}
+					}
+				}()
+
+				go func() {
+					for {
+						msg, ok := <-d.responseCh
+						if !ok {
+							break
+						}
+						if n, err := r.WriteToUDP(msg.msg, senderAddr); err != nil || n != len(msg.msg) {
+							fmt.Printf("error writing to udp socket %v\n", err)
+						}
+					}
+				}()
+			}(reqCh, senderAddr)
 		}
+
+		// p, err := srt.Unmarshal(msg[:n])
+		// if err != nil {
+		// 	fmt.Printf("error unmarshaling srt packet %v\n", err)
+		// 	return
+		// }
+		// if p.DestinationSocketId() == 0 {
+		// 	switch v := p.(type) {
+		// 	case *srt.ControlPacket:
+		// 		sourceLock.Lock()
+		// 		sources[v.HandshakeSocketId()] = senderAddr
+		// 		sourceLock.Unlock()
+		// 	}
+		// }
+		// if err != nil {
+		// 	fmt.Printf("error unmarshalling rtp packet %v\n", err)
+		// 	return
+		// }
+
+		// for _, conn := range conns {
+		// 	if _, err := conn.Write(msg[:n]); err != nil {
+		// 		fmt.Printf("error writing to socket %v: %v\n", conn, err)
+		// 	}
+		// }
+
+		// switch v := p.(type) {
+		// case *srt.DataPacket:
+		// 	fmt.Printf("sending pkt %d\n", v.SequenceNumber())
+		// 	// write to all interfaces.
+		// 	for _, conn := range conns {
+		// 		if _, err := conn.Write(msg[:n]); err != nil {
+		// 			fmt.Printf("error writing to socket %v: %v\n", conn, err)
+		// 		}
+		// 	}
+		// case *srt.ControlPacket:
+		// 	// pick a random connection.
+		// 	fmt.Printf("control pkt\n")
+		// 	conn := conns[0]
+		// 	if _, err := conn.Write(msg[:n]); err != nil {
+		// 		fmt.Printf("error writing to socket %v: %v\n", conn, err)
+		// 	}
+		// }
 	}
 }
 
