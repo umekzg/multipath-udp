66b8448b7bdab78daa5d014bfaa369a1f6f1f52f
Kevin Wang <kevmo314@gmail.com>
2021-04-27 21:23:43 +0000

    z

--- pkg/buffer/sender_buffer.go
+++ pkg/buffer/sender_buffer.go
@@ -1,6 +1,7 @@
 package buffer
 
 import (
+	"fmt"
 	"net"
 
 	"github.com/muxfd/multipath-udp/pkg/srt"
@@ -14,13 +15,18 @@ type Packet struct {
 // SenderBuffer represents a linear buffer that discards based on the sequence number.
 type SenderBuffer struct {
 	buf []*Packet
+	seq uint32 // used for metrics.
 }
 
 func NewSenderBuffer() *SenderBuffer {
-	return &SenderBuffer{buf: make([]*Packet, 1<<24)}
+	return &SenderBuffer{buf: make([]*Packet, 1<<24), seq: 0}
 }
 
 func (b *SenderBuffer) Add(sender *net.UDPAddr, data *srt.DataPacket) {
+	if b.seq != 0 && b.seq+1 != data.SequenceNumber() {
+		fmt.Printf("sequence number jump expect %d got %d (diff %d)\n", b.seq+1, data.SequenceNumber(), data.SequenceNumber()-b.seq-1)
+	}
+	b.seq = data.SequenceNumber()
 	b.buf[int(data.SequenceNumber())%len(b.buf)] = &Packet{Sender: sender, Data: data}
 }
 
